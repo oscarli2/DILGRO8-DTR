8a403ca476c5df2d3377f47b384712e8751aab7d
Oscarlito Oballo <botzock09@gmail.com>
2024-11-08 10:55:43 +0800

    1

--- get_logged_in_count.php
+++ get_logged_in_count.php
@@ -25,7 +25,7 @@ $sql = "
     JOIN Userinfo u ON emp.Userid = u.Userid
     LEFT JOIN Dept d ON u.Deptid = d.Deptid
     WHERE emp.Checktype = 0
-    GROUP BY emp.Userid;
+    GROUP BY emp.Userid, u.Name, d.DeptName;
 ";
 
 $result = $conn->query($sql);
